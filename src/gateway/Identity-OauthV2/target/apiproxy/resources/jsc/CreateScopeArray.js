1cdbc644906c675c597e050240b562cf1701c782
anshul07 <anshul@raremile.com>
2014-04-10 15:58:53 +0530

    User Profile Service
        * code clean up

--- src/gateway/Identity-OauthV2/target/apiproxy/resources/jsc/CreateScopeArray.js
+++
@@ -1,8 +0,0 @@
-var scope = context.getVariable("scope");
-var scopeArray = [];
-
-if (scope != null) {
-	scopeArray = scope.split(" ");
-}
-
-context.setVariable("scopes", JSON.stringify(scopeArray));
--- src/gateway/Identity-OauthV2/target/apiproxy/resources/jsc/ExtractVariablesFromSession.js
+++
@@ -1,7 +0,0 @@
-var sessionJson = context.getVariable("session.Payload");
-var obj = JSON && JSON.parse(sessionJson) || $.parseJSON(sessionJson);
-
-context.setVariable("response_type", obj.response_type);
-context.setVariable("redirect_uri", obj.redirect_uri);
-context.setVariable("scope", obj.scope);
-context.setVariable("client_id", obj.client_id);
