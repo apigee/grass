7a53d114a5db10f17908c326bc0af568406dc55d
Anshul Agarwal <aagrawal@apigee.com>
2014-05-29 11:06:19 +0530

    Update app.js

--- src/gateway/identity-consentmgmt-api-node-module/consentmgmt/app.js
+++ src/gateway/identity-consentmgmt-api-node-module/consentmgmt/app.js
@@ -15,7 +15,6 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
